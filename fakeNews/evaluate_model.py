dbc9807ac544e5003e92b9717fff1f2ca5117b0a
Ashley <ajudson@uccs.edu>
2025-04-12 16:24:03 -0600

    Can test on Test data, 87% accuracy

--- fakeNews/evaluate_model.py
+++ fakeNews/evaluate_model.py
@@ -1,1 +1,23 @@
-# Stage 4: Final Model Evaluation
+''' Stage 4: Final Model Evaluation
+
+Testing model on unseen data to evaluate how the model performs
+'''
+
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+def evaluate_on_test_data(model, test_df):
+    ''' Evaluating the model on the test set '''
+    X_test = test_df['cleaned_text']
+    y_test = test_df['label']
+    print('\tEvaluating on TEST set...')
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    print_test_data_result(acc, y_test, y_pred)
+
+
+def print_test_data_result(acc, y_test, y_pred):
+    print(f"\nTest Accuracy: {acc:.4f}")
+    print("\nClassification Report:")
+    print(classification_report(y_test, y_pred))
+    print("\nConfusion Matrix:")
+    print(confusion_matrix(y_test, y_pred))
